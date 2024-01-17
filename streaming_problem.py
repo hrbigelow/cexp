42075b61cc3dfe4dc1b54558d01c251ebab5f6de
Henry Bigelow <hrbigelow@gmail.com>
2024-01-16 17:43:32 -0800

    update streaming problem

--- streaming_problem.py
+++ streaming_problem.py
@@ -38,16 +38,87 @@ data before actually calling `write_output`)
 
 
 """
+from typing import List
+import random
+import time
 
-def stream(start_vals: List[int], lengths: List[int]) -> None:
-    # pseudo-code
-    mod_val = get_mod_val() # This starts the timer!
-    for i in range(max(lengths)):
-        for idx, start_val in enumerate(start_vals):
-            if i < lengths[idx]:
-                write_output(idx, fibmod(start_val, mod_val, i))
+class Streaming:
+    def __init__(self, start_vals, lengths):
+        self.mod_val = None
+        self.timestamps = []
+        self.indexes = []
+        self.fibmods = []
+        self.start_vals = start_vals
+        self.lengths = lengths
+        self.n = len(self.start_vals)
 
-        
-   
-    
+    def get_mod_val(self):
+        start = time.monotonic()
+        self.timestamps.append(start)
+        self.indexes.append(None)
+        self.fibmods.append(None)
+        self.mod_val = random.choice(range(10000, 50000))
+        return self.mod_val
+
+    def write_output(self, idx, val):
+        self.timestamps.append(time.monotonic())
+        self.indexes.append(idx)
+        self.fibmods.append(val)
+
+    def _fibmod(self, idx):
+        # yield the even subset of the modded fibonacci starting at `start`
+        start = self.start_vals[idx] % self.mod_val
+        n = self.lengths[idx]
+        a, b = start, start
+        for _ in range(n):
+            while a % 2 != 0:
+                a, b = b, (a + b) % self.mod_val
+            yield a 
+
+    def check_order(self):
+        # Call after user calls stream to confirm correctness
+        gens = [self._fibmod(idx) for idx in range(self.n)]
+        z = zip(self.indexes, self.fibmods)
+        next(z) # throw away first value
+        for idx, val in z:
+            try:
+                uval = next(gens[idx])
+            except:
+                return False
+            if val != uval:
+                return False
+        return True
+
+    def find_max_gap(self):
+        # Find maximum time gap in each stream between consecutive outputs 
+        prev_time = [None] * self.n
+        max_gaps = [0] * self.n
+        z = zip(self.indexes, self.timestamps)
+        next(z) # throw away first value
+        for idx, ts in z:
+            if prev_time[idx] is not None:
+                max_gaps[idx] = max(max_gaps[idx], ts - prev_time[idx])
+            prev_time[idx] = ts
+        return max(max_gaps)
+
+# def stream(streaming: Streaming, start_vals: List[int], lengths: List[int]) -> None:
+    # pass
+
+def evaluate(n, stream_fn):
+    start_vals = random.choices(range(50000), k=n)
+    lengths = random.choices(range(1000, 2000), k=n)
+    streaming = Streaming(start_vals, lengths)
+    stream_fn(streaming, start_vals, lengths)
+    print(f'{len(streaming.fibmods)=}')
+    if not streaming.check_order():
+        print(f'Failed ordering check')
+        return False
+    max_time_lag = streaming.find_max_gap()
+    print(f'Maximun time lag: {max_time_lag:0.5f}')
+
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    evaluate(n)
+
 
